174c6d4d23afb182b3ecf6134fe68bcc69581652
ichatworld2023 <135700282+ichatworld2023@users.noreply.github.com>
2023-10-17 14:14:21 +0000

    add name

--- middleware.js
+++ middleware.js
@@ -1,16 +1,42 @@
+import geoip from 'geoip-lite'
+
 export const config = {
-  matcher: '/*',
+  matcher: '/admin',
 }
 
-const ALLOWED_COUNTRIES = ['CN', 'HK']
+// const ALLOWED_COUNTRIES = ['CN', 'HK']
 
 export default function middleware(request) {
   const url = new URL(request.url)
 
-  const country = request.geo.country || 'CN'
+  //   if (url.pathname === '/admin')
+  //     url.pathname = '/redirected'
+  const ip = request.headers.get('x-real-ip')
+  console.log(ip)
+  
+  const geo = geoip.lookup(ip)
+  console.log(geo)
 
-  if (!ALLOWED_COUNTRIES.includes(country))
-    url.pathname = '/blocked'
+  if (!['CN', 'HK'].includes('CN'))
+    console.log('asdfasdf')
 
+  url.pathname = '/redirected'
   return Response.redirect(url)
 }
+
+// export const config = {
+//     matcher: '/*',
+//   }
+
+//   const ALLOWED_COUNTRIES = ['CN', 'HK']
+
+//   export default function middleware(request) {
+//     const url = new URL(request.url)
+
+//     const country = request.geo.country || 'CN'
+
+//     if (!ALLOWED_COUNTRIES.includes(country))
+//       url.pathname = '/blocked'
+
+//     return Response.redirect(url)
+//   }
