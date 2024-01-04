1358cfc3f2397e9b71f727f669a2de9260bcec6a
ichatworld2023 <135700282+ichatworld2023@users.noreply.github.com>
2024-01-04 14:14:46 +0000

    change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
-import { Show, createSignal, onMount } from 'solid-js'
+import { Index, Show, createSignal, onMount } from 'solid-js'
 
-interface InfoType { is_show: number, text: string }
+interface InfoType { is_show: number, text: string, list:array }
 
 export default () => {
-  const [info, setInfo] = createSignal<InfoType>({ is_show: 1, text: '' })
+  const [info, setInfo] = createSignal<InfoType>({ is_show: 1, text: '', list:[] })
 
   onMount(async() => {
     getInfo()
@@ -30,8 +30,19 @@ export default () => {
         </Show>
 
         <Show when={info().is_show === 1}>
-          <div op-60>我们联合GPT4赞助商给大家送福利啦, 每日9点到10点送出8000字的GPT4兑换码, <a class=" gpt-subtitle text-xs" href="https://gpt4f.ninvfeng.xyz">登录</a>兑换就能用
-            <p class="mt-1">{info().text}</p>
+          <div op-60>我们联合GPT4赞助商给大家送福利啦, 每日不定时送出8000字的GPT4兑换码, <a class=" gpt-subtitle text-xs" href="https://gpt4f.ninvfeng.xyz">登录</a>兑换就能用
+            <Index each={info().list}>
+              {(v, i) => (
+                <span>
+                  <Show when={v().status === '正常'}>
+                    {v().code}&nbsp;
+                  </Show>
+                  <Show when={v().status === '已使用'}>
+                    <span class="op-60">{v().code}已用&nbsp;</span>
+                  </Show>
+                </span>
+              )}
+            </Index>
           </div>
         </Show>
 
