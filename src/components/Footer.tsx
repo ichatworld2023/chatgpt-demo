14dcec5fabcfe9c6810a2b44ca38d0365fd507f4
ichatworld2023 <135700282+ichatworld2023@users.noreply.github.com>
2024-03-26 01:59:51 +0000

    change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,12 +35,12 @@ export default () => {
       <div mt-6 text-xs>
         {/* <div op-60>完全免费 无需魔法 无需登录 120次/天</div> */}
 
-{/*         <Show when={info().is_show === 0}> */}
-          <div op-60>受OpenAI政策影响，上游token供应商大幅涨价，网站的维护成本大大增加。在现有token额度用完后，我们可能会暂时切换到谷歌的Gemini Pro模型，同时我们也会继续寻找便宜的GPT3.5渠道。如果您有相关资源欢迎联系我们: free2gpt@gmail.com ，我们将不忘初心，继续为大家提供免费服务, 感谢您的一路陪伴，您的赞赏和分享是我们持续维护的最大动力</div>
-{/*         </Show> */}
+        {/*         <Show when={info().is_show === 0}> */}
+        <div op-60>受OpenAI政策影响，上游token供应商大幅涨价，网站的维护成本大大增加。在现有token额度用完后，我们可能会暂时切换到谷歌的Gemini Pro模型，同时我们也会继续寻找便宜的GPT3.5渠道。如果您有相关资源欢迎联系我们: free2gpt@gmail.com ，我们将不忘初心，继续为大家提供免费服务, 感谢您的一路陪伴，您的赞赏和分享是我们持续维护的最大动力💪</div>
+        {/*         </Show> */}
 
         <Show when={info().is_show === 1}>
-          <div op-60>
+          <div op-60 mt-2>
             <div>
               我们联合GPT4赞助商给大家送福利啦, 每日不定时送出8000字的GPT4兑换码, <a class=" gpt-subtitle text-xs" href="https://gpt4f.ninvfeng.xyz">登录</a>兑换就能用
             </div>
