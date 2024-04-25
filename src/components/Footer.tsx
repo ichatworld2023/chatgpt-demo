424197c92579696c50c38d45c0b4b57c39868ea8
ichatworld2023 <135700282+ichatworld2023@users.noreply.github.com>
2024-04-25 11:21:59 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,17 +39,17 @@ export default () => {
         {/* <div op-60>完全免费 无需魔法 无需登录 120次/天</div> */}
 
         <Show when={host() !== 'llama3.free2gpt.xyz'}>
-          <div op-60>完全免费 无需魔法 无需登录 120次/天 gemini-pro模型, 我们希望能尽快用上llama3 70B模型, 但目前还不稳定, 为此我们在保证gemini-pro稳定的情况下单独搭建 <a class=" gpt-subtitle text-xs" href="https://llama3.free2gpt.xyz">llama3 70B站点</a> 给大家尝鲜, 欢迎试用反馈</div>
+          <div op-60>完全免费 无需魔法 无需登录 120次/天 gemini-pro模型, 试用Meta最新发布的<a class=" gpt-subtitle text-xs" href="https://llama3.free2gpt.xyz">llama3 70B>></a></div>
         </Show>
 
         <Show when={host() === 'llama3.free2gpt.xyz'}>
-          <div op-60>完全免费 无需魔法 无需登录 120次/天 Meta最新发布的llama3 70B模型, 新模型是否好用? 期待您的反馈, 您的赞赏和分享是我们持续维护的最大动力🥹 <a class=" gpt-subtitle text-xs" href="https://chatz.free2gpt.xyz">返回使用gemini-pro模型</a></div>
+          <div op-60>完全免费 无需魔法 无需登录 120次/天 Meta最新发布的llama3 70B模型, 您的赞赏和分享是我们持续维护的最大动力🥹 <a class=" gpt-subtitle text-xs" href="https://chatz.free2gpt.xyz">返回使用gemini-pro模型</a></div>
         </Show>
 
         <Show when={info().is_show === 1}>
           <div op-60 mt-2>
             <div>
-              我们联合GPT4赞助商给大家送福利啦, 每日不定时送出8000字的GPT4兑换码, <a class=" gpt-subtitle text-xs" href="https://gpt4f.ninvfeng.xyz">登录</a>兑换就能用
+              GPT4兑换码, <a class=" gpt-subtitle text-xs" href="https://gpt4f.ninvfeng.xyz">登录</a>兑换就能用😎
             </div>
             <div class="mt-1 flex flex-wrap text-xs">
               <Index each={info().list}>
@@ -71,7 +71,7 @@ export default () => {
         <div mt-2 op-60>收藏不迷路: <a class="gpt-subtitle text-xs" href="https://nav.free2gpt.xyz" target="_blank" rel="noreferrer">https://nav.free2gpt.xyz</a>🌟</div>
         <div mt-2><a class=" gpt-subtitle text-xs" href="https://gpt4f.ninvfeng.xyz">赞助商: 3.5不够聪明? 付费使用GPT4</a>😎</div>
         <div mt-2 onClick={() => { setShow(show() ? 0 : 1) }}>
-          <div op-60>支付宝红包<span class="border-1 px-2 py-1 mx-1 rounded-md transition-colors bg-slate/20 cursor-pointer hover:bg-slate/50">领取</span></div>
+          <div op-60>听说支付宝又有大额红包<span class="border-1 px-2 py-1 mx-1 rounded-md transition-colors bg-slate/20 cursor-pointer hover:bg-slate/50">领取</span></div>
           <Show when={show() === 1}>
             <div my-4>
               <img class="w-4/5 mt-2 max-w-[300px]" src="/hongbao_l.jpg" />
