f903e774759576d641eac90b82bbe5b936f2e23b
ichatworld2023 <135700282+ichatworld2023@users.noreply.github.com>
2024-06-12 03:33:55 +0000

    change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,12 +37,12 @@ export default () => {
     <footer>
       <div mt-6 text-xs>
 
-        <div op-60>完全免费 无需魔法 无需登录 120次/天</div>
+        <div op-60>完全免费 无需魔法 无需登录 120次/天 受服务商影响，当前服务可能不稳定，正在努力恢复中</div>
 
         <div mt-2 op-60>收藏不迷路: <a class="gpt-subtitle text-xs" href="https://nav1.free2gpt.xyz" target="_blank" rel="noreferrer">https://nav1.free2gpt.xyz</a>🌟</div>
         <div mt-2><a class=" gpt-subtitle text-xs" href="https://gpt4f.ninvfeng.xyz">赞助商: 3.5不够聪明? 付费使用GPT4</a>😎</div>
         <div mt-2 onClick={() => { setShow(show() ? 0 : 1) }}>
-{/*           <div op-60>听说支付宝又有大额红包<span class="border-1 px-2 py-1 mx-1 rounded-md transition-colors bg-slate/20 cursor-pointer hover:bg-slate/50">领取</span></div> */}
+          {/*           <div op-60>听说支付宝又有大额红包<span class="border-1 px-2 py-1 mx-1 rounded-md transition-colors bg-slate/20 cursor-pointer hover:bg-slate/50">领取</span></div> */}
           <Show when={show() === 1}>
             <div my-4>
               <img class="w-4/5 mt-2 max-w-[300px]" src="/hongbao_l.jpg" />
