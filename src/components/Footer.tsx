cdc959094c1f6f75174bbd8150b7920c9aa9cb2c
ichatworld2023 <135700282+ichatworld2023@users.noreply.github.com>
2024-10-29 22:00:08 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Index, Show, createSignal, onMount } from 'solid-js'
-// import { useClipboard } from 'solidjs-use'
+import { useClipboard } from 'solidjs-use'
 
 interface InfoType { is_show: number, text: string, list: Array<any> }
 
@@ -9,14 +9,14 @@ export default () => {
   const [show, setShow] = createSignal(0)
   const [remain, setRemain] = createSignal(0)
 
-  // const [source] = createSignal('')
-  // const { copy } = useClipboard({ source, copiedDuring: 1000 })
+  const [source] = createSignal('')
+  const { copy } = useClipboard({ source, copiedDuring: 1000 })
 
-  // const copyAction = async(text) => {
-  //   copy(text).then(() => {
-  //     alert('复制成功')
-  //   })
-  // }
+  const copyAction = async(text) => {
+    copy(text).then(() => {
+      alert('口令复制成功')
+    })
+  }
 
   onMount(async() => {
     getInfo()
@@ -78,16 +78,21 @@ export default () => {
           </div>
         </Show>
 
-        <div mt-2><a href="https://claude.gptnb.xyz"><span class="op-60">👉</span> <span class=" gpt-subtitle text-xs">Claude 3.5 Sonnet(New) 2024年10月22日新模型</span></a></div>
+        <div mt-2><a href="https://claude.gptnb.xyz"><span class="op-60">👉</span> <span class=" gpt-subtitle text-xs">Claude 3.5 Sonnet(New) </span></a></div>
         <div mt-2><a href="https://o1-preview.gptnb.xyz"><span class="op-60">👉</span> <span class=" gpt-subtitle text-xs">o1-preview</span></a></div>
-         <div mt-2 onClick={() => { setShow(show() ? 0 : 1) }}>
+        
+        <div mt-2 onClick={() => { copyAction }}>
+          <div op-60>兄弟们, 拼多多帮我砍一刀, 口令: 2:/⇥R06aMxvkldaJz⇤ <span class="border-1 px-2 py-1 mx-1 rounded-md transition-colors bg-slate/20 cursor-pointer hover:bg-slate/50">复制口令</span></div>
+        </div> 
+        
+{/*         <div mt-2 onClick={() => { setShow(show() ? 0 : 1) }}>
           <div op-60>点击可领支付宝大额红包<span class="border-1 px-2 py-1 mx-1 rounded-md transition-colors bg-slate/20 cursor-pointer hover:bg-slate/50">领取</span></div>
           <Show when={show() === 1}>
             <div my-4>
               <img class="w-4/5 mt-2 max-w-[300px]" src="/hongbao_f.jpg" />
             </div>
           </Show>
-        </div> 
+        </div>  */}
 
       </div>
     </footer>
