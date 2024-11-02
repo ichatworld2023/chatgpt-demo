396de457ec50bd6077db731c85e8d1e900b72055
ichatworld2023 <135700282+ichatworld2023@users.noreply.github.com>
2024-11-02 14:39:31 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -81,11 +81,11 @@ export default () => {
         <div mt-2><a href="https://claude.gptnb.xyz"><span class="op-60">👉</span> <span class=" gpt-subtitle text-xs">Claude 3.5 Sonnet(New) </span></a></div>
         <div mt-2><a href="https://o1-preview.gptnb.xyz"><span class="op-60">👉</span> <span class=" gpt-subtitle text-xs">o1-preview</span></a></div>
         
-        <div mt-2 onClick={() => { copyAction('2:/⇥R06aMxvkldaJz⇤') }}>
+{/*         <div mt-2 onClick={() => { copyAction('2:/⇥R06aMxvkldaJz⇤') }}>
           <div op-60>兄弟们, 拼多多帮我砍一刀</div>
           <div op-60 mt-1> 2:/⇥R06aMxvkldaJz⇤ </div>
           <div op-60 mt-1><span class="border-1 px-2 py-1 rounded-md transition-colors bg-slate/20 cursor-pointer hover:bg-slate/50">复制口令</span></div>
-        </div> 
+        </div>  */}
         
 {/*         <div mt-2 onClick={() => { setShow(show() ? 0 : 1) }}>
           <div op-60>点击可领支付宝大额红包<span class="border-1 px-2 py-1 mx-1 rounded-md transition-colors bg-slate/20 cursor-pointer hover:bg-slate/50">领取</span></div>
